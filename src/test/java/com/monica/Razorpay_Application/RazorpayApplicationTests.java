cd085337a53953aff8fa518dde8ed1048d99fb64
monica-muthukumaran <monicamuthukumaran28@gmail.com>
2026-08-12 05:34:17 +0530

    initial setup

---
+++ src/test/java/com/monica/Razorpay_Application/RazorpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monica.Razorpay_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
